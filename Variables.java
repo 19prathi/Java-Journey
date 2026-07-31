db3aa676221732f63988c754aa12cc2572d0129c
19prathi <prathi1910m@gmail.com>
2026-07-31 16:23:22 +0530

    Variables

---
+++ Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main (String[] args) {
+        int a =1000;
+        int b=90;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
