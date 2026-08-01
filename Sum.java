0da04c1e6029bb3e066cd8ca25830958bbb24f0a
PRATHIK M <prathi1910m@gmail.com>
2026-08-01 15:30:03 +0530

    Added a comment to Sum.java

--- Sum.java
+++ Sum.java
@@ -1,5 +1,6 @@
 public class Sum {
     public static void main (String[] args){
+        // Code to calcuate the sum of 2 numbers 
         int a = 1000;
         int b = 999;
         int sum = a+b;
