9fb93385860aea90d721f8dcb448e9570e664564
PRATHIK M <prathi1910m@gmail.com>
2026-08-01 16:04:02 +0530

    Inputs

---
+++ Inputs.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Inputs {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        Boolean is  = sc.nextBoolean();
+        System.out.println(is);
+    }
+}
