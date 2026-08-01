d3029d244ec131048a89258178ebac9316121cce
PRATHIK M <prathi1910m@gmail.com>
2026-08-01 15:06:17 +0530

    Datatypes

---
+++ Datatypes.java
@@ -0,0 +1,15 @@
+public class Datatypes {
+    public static void main (String[] args){
+        int a = 5;
+        float f = 3.14f;
+        boolean b = true;
+        long q = 345573;
+        double p = 3.142857;
+        System.out.println(a);
+        System.out.println(f);
+        System.out.println(b);
+        System.out.println(q);
+        System.out.println(p);
+    }
+    
+}
---
+++ Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+    public static void main (String[] args){
+        int a =100;
+        int b=98;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+
+    
+}
