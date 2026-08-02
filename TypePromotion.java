351ad75ae64d6ab7a9bcfbdb85becb502627bfa8
PRATHIK M <prathi1910m@gmail.com>
2026-08-02 18:34:16 +0530

    Type.promotion.java

---
+++ TypePromotion.java
@@ -0,0 +1,8 @@
+public class TypePromotion {
+    public static void main(String[] args){
+        char a = 'a';
+        short b = 50;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
