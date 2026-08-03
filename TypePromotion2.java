e6976a8dcc71a3a85963a704dbc5a75d4b484873
PRATHIK M <prathi1910m@gmail.com>
2026-08-03 20:57:56 +0530

    TypePromotion2.java

---
+++ TypePromotion2.java
@@ -0,0 +1,11 @@
+public class TypePromotion2 {
+    public static void main(String[] args){
+        int a = 10;
+        float b = 12.43f;
+        long c = 45;
+        double d = 3000;
+        double ans = a+b+c+d;
+        System.out.println(ans);
+    }
+    
+}
