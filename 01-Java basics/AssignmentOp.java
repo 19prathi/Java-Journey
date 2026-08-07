a5ca2c86bde6283c60667d76376e2a1ede3ac084
PRATHIK M <prathi1910m@gmail.com>
2026-08-07 19:50:26 +0530

    AssignmentOp.java

---
+++ 01-Java basics/AssignmentOp.java
@@ -0,0 +1,9 @@
+public class AssignmentOp {
+    public static void main(String[] args){
+        int x=2;
+        int y=5;
+        int Exp=(x*y/x);
+        System.out.println(Exp); 
+    }
+    
+}
