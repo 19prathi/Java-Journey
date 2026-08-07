917708177a3b59fe57dbe72a471e147abf19d76d
PRATHIK M <prathi1910m@gmail.com>
2026-08-07 16:11:45 +0530

    Unary.java

--- 01-Java basics/UnaryOp.java
+++ 01-Java basics/UnaryOp.java
@@ -1,9 +1,13 @@
-public class PreIncrement {
+public class UnaryOp {
     public static void main( String[] args){
         int a = 10;
         int b = ++a;
+        int c = --a;
+        int d = a--;
         System.out.println(a);
         System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
 
     }
     
