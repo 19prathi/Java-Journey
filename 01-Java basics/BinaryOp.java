a4c076194278c5de3fda64af475578a6a1f8f657
PRATHIK M <prathi1910m@gmail.com>
2026-08-07 15:34:14 +0530

    BinaryOp.java

---
+++ 01-Java basics/BinaryOp.java
@@ -0,0 +1,13 @@
+public class BinaryOp {
+    public static void main(String[] args){
+        int A = 10;
+        int B = 99;
+        System.out.println("Sum = " + (A+B));
+        System.out.println("Diff = " + (A-B));
+        System.out.println("Multiply = " +(A*B));
+        System.out.println("Divide = " +(A/B));
+        System.out.println("Modulo(remainder) = " +(A%B));
+
+    }
+    
+}
