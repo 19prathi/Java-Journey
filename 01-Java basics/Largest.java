69a3d2b625c25118914fcd4f55fbd933f7e9c577
PRATHIK M <prathi1910m@gmail.com>
2026-08-10 16:40:31 +0530

    Largest.java

---
+++ 01-Java basics/Largest.java
@@ -0,0 +1,12 @@
+public class Largest {
+    public static void main (String[] args) {
+        int a = 50;
+        int b = 120;
+        if (a > b) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+    }
+
+}
