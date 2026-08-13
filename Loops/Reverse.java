7e7a13da98b01c3fc8c20f15c6a7689a77e0a20c
PRATHIK M <prathi1910m@gmail.com>
2026-08-13 14:06:19 +0530

    Reverse.java

---
+++ Loops/Reverse.java
@@ -0,0 +1,12 @@
+public class Reverse {
+    public static void main(String[] args){
+        int n = 191006;
+        int rev = 0;
+        while(n >0){
+            int lastDigit = n % 10;
+            rev = (rev *10) + lastDigit;
+            n = n/10;  
+        }
+     System.out.println(rev);
+    }    
+}
