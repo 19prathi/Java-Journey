3d04290cc8ba46fa9e7f9222286e31342cbc090b
PRATHIK M <prathi1910m@gmail.com>
2026-08-13 15:23:45 +0530

    PrimeNumber.java

--- Loops/PrimeNumber.java
+++ Loops/PrimeNumber.java
@@ -3,16 +3,13 @@ public class PrimeNumber {
     public static void main(String[] args){
         Scanner sc =  new Scanner(System.in);
         int n = sc.nextInt();
-        if (n <= 1) {
-            System.out.println("n is Not Prime");
-        } else if (n == 2) {
+        if (n ==2 ) {
             System.out.println("n is Prime");
-        } else {
+        }else {
             boolean isPrime = true;
-            for (int i = 2; i <= Math.sqrt(n); i++) {
+            for (int i = 2; i <= n-1; i++) {
                 if (n % i == 0) {
                     isPrime = false;
-                    break;
                 }
             }
             if (isPrime) {
@@ -22,7 +19,4 @@ public class PrimeNumber {
             }
         }
     }
-
 }
-
-      
