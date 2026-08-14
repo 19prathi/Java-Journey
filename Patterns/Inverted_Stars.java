34140a3b14fbe76031d01b73433bb7765ea39207
PRATHIK M <prathi1910m@gmail.com>
2026-08-14 16:10:17 +0530

    Inverted_Stars.java

---
+++ Patterns/Inverted_Stars.java
@@ -0,0 +1,13 @@
+package Patterns;
+
+public class Inverted_Stars {
+    public static void main(String[] args){
+        int n =4;
+        for(int i = 1; i <= 4; i++){
+            for(int s = 1; s <= (n-i+1); s++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
