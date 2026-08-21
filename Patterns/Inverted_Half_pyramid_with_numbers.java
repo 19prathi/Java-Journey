9a6113294f20506287ef2b137e46148e3ab87c62
PRATHIK M <prathi1910m@gmail.com>
2026-08-21 20:33:48 +0530

    Inverted_Half_pyramid_with_numbers.java

---
+++ Patterns/Inverted_Half_pyramid_with_numbers.java
@@ -0,0 +1,15 @@
+package Patterns;
+
+public class Inverted_Half_pyramid_with_numbers {
+    public static void printpattern(int n){
+        for( int i = 1; i <= n; i++){
+            for( int j = 1; j <= n-i+1; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        printpattern(7);
+    }
+}
