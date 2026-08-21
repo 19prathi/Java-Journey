e40c91069e91ca0586f88572951fa33be8d87a4a
PRATHIK M <prathi1910m@gmail.com>
2026-08-21 20:11:05 +0530

    Hallow_Rectangle.java

--- Patterns/Hollow_Rectangle.java
+++ Patterns/Hollow_Rectangle.java
@@ -1,16 +1,20 @@
-public static void Hollow_Rectangle( int totRows, int totCols){
-    for(int i = 1; i <= totRows; i++){
-        for(int j = 1; j <= totCols; j++){
-            if( i == 1 || i == totRows || j == 1 || j == totCols){
-                System.out.print("*");
+package Patterns;
+
+public class Hollow_Rectangle {
+    public static void Hollow_Rectangle(int totRows, int totCols) {
+        for (int i = 1; i <= totRows; i++) {
+            for (int j = 1; j <= totCols; j++) {
+                if (i == 1 || i == totRows || j == 1 || j == totCols) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
-            else{
-                System.out.print(" ");
-            }
+            System.out.println();
         }
-        System.out.println();
     }
-}
-public static void main(String[] args){
-    Hollow_Rectangle(11, 9);
+
+    public static void main(String[] args) {
+        Hollow_Rectangle(11, 9);
+    }
 }
