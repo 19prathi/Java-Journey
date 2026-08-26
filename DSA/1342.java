46df774ccbe643e1c8ab5b297d8acd401bb16256
PRATHIK M <prathi1910m@gmail.com>
2026-08-26 15:18:10 +0530

    1342.java

---
+++ DSA/1342.java
@@ -0,0 +1,16 @@
+package DSA;
+
+class Solution {
+    public int numberOfSteps(int num) {
+        int steps = 0; 
+        while( num != 0){
+            if(num % 2 == 0){
+                num =  num / 2;
+            }else{
+                num = num - 1;
+            }
+            steps++;
+        }
+        return steps;
+    }
+}
