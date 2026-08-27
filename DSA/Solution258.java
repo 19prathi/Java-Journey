d035e522e236f83b8eb6f520369926c1e372ee64
PRATHIK M <prathi1910m@gmail.com>
2026-08-27 18:05:46 +0530

    LinearSearch.java

---
+++ DSA/Solution258.java
@@ -0,0 +1,17 @@
+package DSA;
+
+public class Solution258 {
+
+    public int addDigits(int num) {
+        while(num >= 10){
+            int sum = 0;
+            while(num > 0){
+                int digit = num % 10;
+                sum = sum + digit;
+                num = num / 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
