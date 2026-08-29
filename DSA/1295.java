3166a0087c4d969f86321e8b6c6e6d7791f71964
PRATHIK M <prathi1910m@gmail.com>
2026-08-29 20:02:19 +0530

    1295.java

---
+++ DSA/1295.java
@@ -0,0 +1,25 @@
+package DSA;
+
+class Solution {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+
+        for (int num : nums) {
+            int digits = 0;
+
+            if (num == 0) {
+                digits = 1;
+            } else {
+                while (num > 0) {
+                    num /= 10;
+                    digits++;
+                }
+            }
+
+            if (digits % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
